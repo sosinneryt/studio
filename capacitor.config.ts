992d5e2dcb2971f8e4a690a8915fc4d5a0a4e84d
Swasstik Mohit Pandey <sosinnerswastik@gmail.com>
2026-02-07 04:56:57 +0000

    well

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.senseoasis.app',
   appName: 'SenseOasis',
-  webDir: 'out'
+  webDir: 'out',
+  android: {
+    buildOptions: {
+      kotlinVersion: '1.8.22'
+    }
+  }
 };
 
 export default config;
