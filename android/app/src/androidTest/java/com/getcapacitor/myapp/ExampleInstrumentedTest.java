67f66fab427936e41e3ad768dd37a5bc80c89e70
Swasstik Mohit Pandey <sosinnerswastik@gmail.com>
2026-02-07 06:30:07 +0000

    help bro

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++
@@ -1,9 +0,0 @@
-package com.getcapacitor.myapp;
-
-/**
- * This file is intentionally left blank to prevent build issues during the APK generation.
- * Test classes can be restored for local development and testing.
- */
-public class ExampleInstrumentedTest {
-    // Intentionally empty.
-}
